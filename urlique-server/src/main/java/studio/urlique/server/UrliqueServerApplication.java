8563a8ac1f05a28f7ab35b938dd0688c90385f99
qrowned <admin@qrowned.dev>
2023-04-27 12:14:43 +0200

    Initial commit

---
+++ urlique-server/src/main/java/studio/urlique/server/UrliqueServerApplication.java
@@ -0,0 +1,13 @@
+package studio.urlique.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrliqueServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrliqueServerApplication.class);
+    }
+
+}
