20fb2483ae9546afa6c19768efd2d38086ed6654
qrowned <admin@qrowned.dev>
2023-05-14 23:20:34 +0200

    Commented UrlDataService

--- urlique-server/src/main/java/studio/urlique/server/url/UrlDataService.java
+++ urlique-server/src/main/java/studio/urlique/server/url/UrlDataService.java
@@ -18,44 +18,79 @@ public final class UrlDataService {
 
     private final UrlDataFirestoreRepository urlDataFirestoreRepository;
 
+    /**
+     * Create a URL data entry without specific ID.
+     *
+     * @see UrlDataService#createUrlDataEntry(String, String, Principal)
+     */
     public CompletableFuture<RequestResult<UrlData>> createUrlDataEntry(@NotNull String url,
                                                                         @Nullable Principal creator) {
         return this.createUrlDataEntry(RandomStringUtils.randomAlphanumeric(6), url, creator);
     }
 
+    /**
+     * Create a URL data entry with specific ID.
+     *
+     * @param id specific ID of short link.
+     * @param url URL the short link should point to.
+     * @param principal creator of the entry/request.
+     * @return result of create request.
+     */
     public CompletableFuture<RequestResult<UrlData>> createUrlDataEntry(@NotNull String id,
                                                                         @NotNull String url,
-                                                                        @Nullable Principal creator) {
+                                                                        @Nullable Principal principal) {
         return this.urlDataFirestoreRepository.get(id).thenComposeAsync(existingData -> {
-            if (existingData.isPresent()) return this.createUrlDataEntry(url, creator);
+            if (existingData.isPresent()) return this.createUrlDataEntry(url, principal);
 
-            UrlData urlData = new UrlData(id, url, creator == null ? null : creator.getName());
+            UrlData urlData = new UrlData(id, url, principal == null ? null : principal.getName());
             this.urlDataFirestoreRepository.save(urlData);
             return CompletableFuture.supplyAsync(() -> RequestResult.ok(urlData));
         });
     }
 
+    /**
+     * Fetch a URL data entry by its ID.
+     *
+     * @param id ID of URL data entry.
+     * @return result of fetch request.
+     */
     public CompletableFuture<RequestResult<UrlData>> fetchUrlDataEntry(@NotNull String id) {
         return this.urlDataFirestoreRepository.get(id).thenApplyAsync(urlDataOptional -> {
             if (urlDataOptional.isEmpty()) return RequestResult.error("url.id.notFound");
 
-            UrlData urlData = urlDataOptional.get();
-
-            return RequestResult.ok(urlData);
+            return RequestResult.ok(urlDataOptional.get());
         });
     }
 
-    public CompletableFuture<RequestResult<List<UrlData>>> fetchUrlDataEntries(@NotNull Principal creator, int pageSize, int page) {
+    /**
+     * Fetch all URL data entries of a certain creator.
+     * To prevent memory overload, a pagination system is implemented.
+     *
+     * @param creator creator to get the URL data entries from.
+     * @param pageSize how many entries should be on one page.
+     * @param page specific page to fetch from.
+     * @return result of fetch request.
+     */
+    public CompletableFuture<RequestResult<List<UrlData>>> fetchUrlDataEntries(@NotNull Principal creator,
+                                                                               int pageSize, int page) {
         if (pageSize > 50) return CompletableFuture.completedFuture(RequestResult.error("url.pageSize.tooLarge"));
         return this.urlDataFirestoreRepository.retrieveAllByCreator(creator.getName(), pageSize, page).thenApplyAsync(RequestResult::ok);
     }
 
-    public CompletableFuture<RequestResult<UrlData>> deleteUrlDataEntry(@NotNull String id, @NotNull Principal creator) {
+    /**
+     * Delete a specific URL data entry.
+     *
+     * @param id ID of URL data entry which should be deleted.
+     * @param principal identity who executed the request.
+     * @return result of delete request.
+     */
+    public CompletableFuture<RequestResult<UrlData>> deleteUrlDataEntry(@NotNull String id,
+                                                                        @NotNull Principal principal) {
         return this.urlDataFirestoreRepository.get(id).thenApplyAsync(urlDataOptional -> {
             if (urlDataOptional.isEmpty()) return RequestResult.error("url.id.notFound");
 
             UrlData urlData = urlDataOptional.get();
-            if (urlData.equalsCreator(creator.getName()))
+            if (urlData.equalsCreator(principal.getName()))
                 return RequestResult.error("url.action.noPermission");
 
             this.urlDataFirestoreRepository.delete(id);
