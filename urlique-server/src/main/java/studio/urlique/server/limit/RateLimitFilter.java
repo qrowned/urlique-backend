77844df32b946f0aca183fd593197a6172caaea7
qrowned <admin@qrowned.dev>
2023-06-14 22:51:34 +0200

    Change RateLimitFilter to OncePerRequestFilter

--- urlique-server/src/main/java/studio/urlique/server/limit/RateLimitFilter.java
+++ urlique-server/src/main/java/studio/urlique/server/limit/RateLimitFilter.java
@@ -4,15 +4,14 @@ import io.github.bucket4j.Bucket;
 import io.github.bucket4j.ConsumptionProbe;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.apachecommons.CommonsLog;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.security.Principal;
@@ -20,17 +19,14 @@ import java.security.Principal;
 @Component
 @CommonsLog
 @RequiredArgsConstructor
-public final class RateLimitFilter extends GenericFilterBean {
+public final class RateLimitFilter extends OncePerRequestFilter {
 
     private final RoleLimitService roleLimitService;
 
     @Override
-    public void doFilter(ServletRequest servletRequest,
-                         ServletResponse servletResponse,
-                         FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
-        HttpServletResponse response = (HttpServletResponse) servletResponse;
-
+    protected void doFilterInternal(@NotNull HttpServletRequest request,
+                                    @NotNull HttpServletResponse response,
+                                    @NotNull FilterChain filterChain) throws ServletException, IOException {
         String forwardedFor = request.getHeader("X-Forwarded-For");
         String ipAddress = forwardedFor == null ? request.getRemoteAddr() : forwardedFor.split(",")[0];
 
@@ -44,13 +40,12 @@ public final class RateLimitFilter extends GenericFilterBean {
         ConsumptionProbe consumptionProbe = bucket.tryConsumeAndReturnRemaining(1);
         if (consumptionProbe.isConsumed()) {
             response.addHeader("X-Rate-Limit-Remaining", String.valueOf(consumptionProbe.getRemainingTokens()));
-            filterChain.doFilter(servletRequest, servletResponse);
+            filterChain.doFilter(request, response);
         } else {
             long waitForRefill = consumptionProbe.getNanosToWaitForRefill() / 1_000_000_000;
             response.addHeader("X-Rate-Limit-Retry-After-Seconds", String.valueOf(waitForRefill));
             response.sendError(HttpStatus.TOO_MANY_REQUESTS.value(), "You've reached your Request quota.");
         }
-
     }
 
 }
